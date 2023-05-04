a6c22b5011961ddbf6af62b5e709accb35887010
John Ragland <jhrag@uw.edu>
2023-05-04 16:07:19 +0000

    some work on simulating and modelling depth change

--- simulation/simulate_depth_difference.py
+++ simulation/simulate_depth_difference.py
@@ -33,49 +33,72 @@ ssp_s_interp = xr.concat((ssp_s.loc[:1523], ssp_s.loc[1524:1534].interp({'depth'
 pts_m = []
 pts_s = []
 
-del_depths = np.arange(-5, 5, 0.1)
+del_depths = np.arange(0, 5, 0.25)
+
+depth_cc = 1523
+depth_ec = 1523
+bottom_depth = 1524
+
+del_depths_cc = []
+del_depths_ec = []
 
 for k in tqdm(range(len(del_depths))):
+    for n in range(len(del_depths)):
+        del_depths_cc.append(del_depths[k])
+        del_depths_ec.append(del_depths[n])
+        
+        ssp_m = pyat_tools.convert_SSP_arlpy(ssp_m_interp, k)
+        #ssp_s = pyat_tools.convert_SSP_arlpy(ssp_s_interp, k)
 
-    ssp_m = pyat_tools.convert_SSP_arlpy(ssp_m_interp, k)
-    ssp_s = pyat_tools.convert_SSP_arlpy(ssp_s_interp, k)
+        fn_m = 'kraken_files/caldera_march'
+        #fn_s = 'kraken_files/caldera_septe'
 
-    fn_m = 'kraken_files/caldera_march'
-    fn_s = 'kraken_files/caldera_septe'
+        pyat_tools.write_env_file_pyat(ssp_m, bottom_depth, depth_cc+del_depths[k], np.array(
+            [cc_ec]), np.array([depth_ec + del_depths[n]]), 10, 'caldera_simulation_march', fn=fn_m, verbose=False)
+        #pyat_tools.write_env_file_pyat(ssp_s, bottom_depth+del_depths[k], depth_cc+del_depths[k], np.array(
+            #[cc_ec]), np.array([depth_ec + del_depths[k]]), 10, 'caldera_simulation_september', fn=fn_s, verbose=False)
 
-    pyat_tools.write_env_file_pyat(ssp_m, 1529+del_depths[k], 1528+del_depths[k], np.array(
-        [cc_ec]), np.array([1519 + del_depths[k]]), 10, 'caldera_simulation_march', fn=fn_m, verbose=False)
-    pyat_tools.write_env_file_pyat(ssp_s, 1529+del_depths[k], 1528+del_depths[k], np.array(
-        [cc_ec]), np.array([1519 + del_depths[k]]), 10, 'caldera_simulation_september', fn=fn_s, verbose=False)
+        # Write field flp file
+        s_depths = np.array([depth_cc + del_depths[k]])  # meters
+        ranges = np.array([cc_ec])  # km
+        r_depths = np.array([depth_ec + del_depths[n]])  # meters
 
-    # Write field flp file
-    s_depths = np.array([1528 + del_depths[k]])  # meters
-    ranges = np.array([cc_ec])  # km
-    r_depths = np.array([1519 + del_depths[k]])  # meters
+        pyat_tools.write_flp_file(s_depths, ranges, r_depths, fn_m)
+        #pyat_tools.write_flp_file(s_depths, ranges, r_depths, fn_s)
 
-    pyat_tools.write_flp_file(s_depths, ranges, r_depths, fn_m)
-    pyat_tools.write_flp_file(s_depths, ranges, r_depths, fn_s)
+        data_lens = {'s_depths': len(s_depths), 'ranges': len(
+            ranges), 'r_depths': len(r_depths)}
+        pf_m = pyat_tools.simulate_FDGF(
+            fn_m, freq_half, [0, 100], 'multiprocessing/', data_lens, True)
+        #pf_s = pyat_tools.simulate_FDGF(
+        #    fn_s, freq_half, [0, 100], 'multiprocessing/', data_lens, True)
 
-    data_lens = {'s_depths': len(s_depths), 'ranges': len(
-        ranges), 'r_depths': len(r_depths)}
-    pf_m = pyat_tools.simulate_FDGF(
-        fn_m, freq_half, [0, 100], 'multiprocessing/', data_lens, True)
-    pf_s = pyat_tools.simulate_FDGF(
-        fn_s, freq_half, [0, 100], 'multiprocessing/', data_lens, True)
+        pt_m = np.real(scipy.fft.ifft(pf_m[:, 0, 0, 0]))
+        #pt_s = np.real(scipy.fft.ifft(pf_s[:, 0, 0, 0]))
 
-    pt_m = np.real(scipy.fft.ifft(pf_m[:, 0, 0, 0]))
-    pt_s = np.real(scipy.fft.ifft(pf_s[:, 0, 0, 0]))
+        pts_m.append(pt_m)
+        #pts_s.append(pt_s)
 
-    pts_m.append(pt_m)
-    pts_s.append(pt_s)
+results = xr.Dataset({
+    'pts': xr.DataArray(np.array(pts_m), dims=['delta_depth', 'time']),
+    'del_depths_cc': xr.DataArray(np.array(del_depths_cc), dims='delta_depth'),
+    'del_depths_ec': xr.DataArray(np.array(del_depths_ec), dims='delta_depth')
+})
 
+fn = '/datadrive/simulation/caldera_inversion_depth_differences_seperate.nc'
+results.to_netcdf(fn)
+
+"""
 # Construct DataArray from simulation and save to disc
 TDGFs_march = xr.DataArray(np.array(pts_m), dims=['delta_depth', 'time'], coords={
                            'delta_depth': del_depths, 'time': t}, name='march')
-TDGFs_september = xr.DataArray(np.array(pts_s), dims=['delta_depth', 'time'], coords={
-                               'delta_depth': del_depths, 'time': t}, name='septemember')
+#TDGFs_september = xr.DataArray(np.array(pts_s), dims=['delta_depth', 'time'], coords={
+#                              'delta_depth': del_depths, 'time': t}, name='septemember')
 
 TDGFs = xr.Dataset({'march': TDGFs_march, 'september': TDGFs_september})
+TDGFs = TDGFs.assign_attrs(depth_cc=depth_cc, depth_ec=depth_ec, bottom_depth=bottom_depth)
 
-fn = '/datadrive/simulation/caldera_inversion_depth_differences.nc'
+fn = '/datadrive/simulation/caldera_inversion_depth_differences_simple.nc'
+
 TDGFs.to_netcdf(fn)
+"""
