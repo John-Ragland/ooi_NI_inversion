09be06488b2cb72ebe333f71b945d3afcd92a7b3
John Ragland <jhrag@uw.edu>
2023-05-24 14:59:38 +0000

    research updates

--- scripts/calculate_arrival_times.py
+++ scripts/calculate_arrival_times.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
     print(cluster.dashboard_link)
     client = Client(cluster)
 
-    for threshold in tqdm([3,4,5]):
+    for threshold in tqdm([3,4,5,100]):
         fn = '/datadrive/NCCFs/1hr_20150101_20230101_ec_cc.nc'
         NCCFs = xr.open_dataarray(fn).chunk({'delay':11999, 'time':100})
         NCCFs = NCCFs.rename({'time':'dates'})
@@ -35,9 +35,9 @@ if __name__ == "__main__":
         NCCFs[~mask,:] = np.nan
 
         ## Compute arrival times 
-        NCCFs201 = NCCFs.rolling(dates=201, min_periods=100).mean()
-        NCCFs601 = NCCFs.rolling(dates=601, min_periods=100).mean()
-        NCCFs1001 = NCCFs.rolling(dates=1001, min_periods=100).mean()
+        NCCFs201 = NCCFs.rolling(dates=201, min_periods=100, center=True).mean()
+        NCCFs601 = NCCFs.rolling(dates=601, min_periods=100, center=True).mean()
+        NCCFs1001 = NCCFs.rolling(dates=1001, min_periods=100, center=True).mean()
 
         NCCFs_avg = [NCCFs201, NCCFs601, NCCFs1001]
 
@@ -67,9 +67,9 @@ if __name__ == "__main__":
         avg_hours = [201, 601, 1001]
 
         for k in range(3):
-            filtered_NCCFs_A.append(xrsignal.filtfilt(NCCFs_avg[k], dim='delay', b=bs_A[k], a=as_A[k]))
+            filtered_NCCFs_A.append(xrsignal.hilbert_mag(xrsignal.filtfilt(NCCFs_avg[k], dim='delay', b=bs_A[k], a=as_A[k]), dim='delay'))
             filtered_NCCFs_A[-1].attrs = {'fc':fcs[k][0], 'avg_hours':avg_hours[k]}
-            filtered_NCCFs_B.append(xrsignal.filtfilt(NCCFs_avg[k], dim='delay', b=bs_B[k], a=as_B[k]))
+            filtered_NCCFs_B.append(xrsignal.hilbert_mag(xrsignal.filtfilt(NCCFs_avg[k], dim='delay', b=bs_B[k], a=as_B[k]), dim='delay'))
             filtered_NCCFs_B[-1].attrs = {'fc':fcs[k][1], 'avg_hours':avg_hours[k]}
 
         ats = []
@@ -82,6 +82,9 @@ if __name__ == "__main__":
 
         ats_x = xr.concat(ats, dim='avg_hour').assign_coords({'avg_hour':[201, 201, 601, 601, 1001, 1001]})
 
-        fn = f'/datadrive/NCCFs/arrival_times/outlier_removal_threshold_{threshold}.nc'
+        if threshold == 100:
+            fn = '/datadrive/NCCFs/arrival_times/no_outlier_removal.nc'
+        else:
+            fn = f'/datadrive/NCCFs/arrival_times/outlier_removal_threshold_{threshold}.nc'
         ats_x.to_netcdf(fn)
     
